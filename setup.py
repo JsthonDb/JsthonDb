f824648dc3020e322385c5995777e6e952718ecc
terribleMOTHMAN <65505901+terribleMOTHMAN@users.noreply.github.com>
2022-12-17 11:25:25 +0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='jsthon',
+      version='0.1',
+      description='Easy to use, fast, productive json database for python',
+      packages=['jsthon'],
+      licence='MIT License',
+      author = 'terribleMOTHMAN',
+      author_email='paradox.smirnoff@gmail.com',
+      url="https://github.com/terribleMOTHMAN/JsthonDb",
+      zip_safe=False)
