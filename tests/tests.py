dbb2ed026066ebeb26cc08ed3bc63a9518533531
Kirill Smirnov <65505901+terribleMOTHMAN@users.noreply.github.com>
2023-03-15 19:24:33 +0400

    Add files via upload

--- tests/tests.py
+++ tests/tests.py
@@ -1,65 +1,96 @@
-from jsthon import JsthonDb
+# from jsthon import JsthonDb
 
-db = JsthonDb('main.json')
+# db = JsthonDb('main.json')
 
-db.create_table('tvshows')
-db.create_table('films')
+# db.create_table('tvshows')
+# db.create_table('films')
+#
+# db.choose_table('tvshows')
+#
+# id = db.add({'name': 'Breaking Bad', 'start': 2008})
+# print(id)
+# id = db.add({'name': 'Mr. Robot', 'start': 2015}, "1")
+# print(id)
 
-db.choose_table('tvshows')
+# added_values = db.add_many([{'name': 'Shameless', 'start': 2011}, {'name': 'The Boys', 'start': 2019}])
+# print(added_values)
+# added_values = db.add_many([{'name': 'Scrubs', 'start': 2001}, {'name': 'How I Met Your Mother', 'start': 2005}], ("0", "2"))
+# print(added_values)
 
-id = db.add({'name': 'Breaking Bad', 'start': 2008})
-print(id)
-id = db.add({'name': 'Mr. Robot', 'start': 2015}, "1")
-print(id)
+# all = db.take_all()
+# print(all)
 
-added_values = db.add_many([{'name': 'Shameless', 'start': 2011}, {'name': 'The Boys', 'start': 2019}])
-print(added_values)
-added_values = db.add_many([{'name': 'Scrubs', 'start': 2001}, {'name': 'How I Met Your Mother', 'start': 2005}], ("0", "2"))
-print(added_values)
+# element = db.take_by_id("1")
+# print(element)
 
-all = db.take_all()
-print(all)
+# def func1(data):
+#     if data['start'] > 2010:
+#         return True
+#
+#
+# print(db.take_with_function(func1))
 
-element = db.take_by_id("1")
-print(element)
+# updated_data = db.update_by_id("1", {'name': 'Better Call Saul'})
+# print(updated_data)
+# print(db.take_by_id("1"))
 
-def func1(data):
-    if data['start'] > 2010:
-        return True
+# def func2(data):
+#     if data['name'] == 'Shameless':
+#         return True
+#
+# updated_data = db.update_with_function(func2, {'name': '$hameless'})
+# print(updated_data)
 
+# deleted_data = db.delete_by_id("2")
+# print(deleted_data)
 
-print(db.take_with_function(func1))
+# def func3(data):
+#     if data['start'] < 2015:
+#         return True
+#
+#
+# deleted_data = db.delete_with_function(func3)
+# print(deleted_data)
 
-updated_data = db.update_by_id("1", {'name': 'Better Call Saul'})
-print(updated_data)
-print(db.take_by_id("1"))
+# db.add_new_key('broadcast', True)
 
-def func2(data):
-    if data['name'] == 'Shameless':
-        return True
+# db.add_new_keys(['ratings', 'language'], ['good', 'english'])
 
-updated_data = db.update_with_function(func2, {'name': '$hameless'})
-print(updated_data)
-
-deleted_data = db.delete_by_id("2")
-print(deleted_data)
-
-def func3(data):
-    if data['start'] < 2015:
-        return True
-
-
-deleted_data = db.delete_with_function(func3)
-print(deleted_data)
-
-db.add_new_key('broadcast', True)
-print(db.take_all())
-
-db.add_new_keys(['ratings', 'language'], ['good', 'english'])
-print(db.take_all())
-
-a = db.show_table()
-print(a)
+# a = db.show_table()
+# print(a)
 
 # db.clear_table()
-db.clear_db()
+# db.clear_db()
+
+# password = 'themostcommonpassword'
+#
+# db = JsthonDb('main.json')
+#
+# db.set_encryption(True)
+# db.set_encryption_keys(password)
+#
+# db.create_table('tvshows')
+# db.create_table('films')
+#
+# db.choose_table('tvshows')
+#
+# db.add({'name': 'Breaking Bad', 'start': 2008})
+# db.set_encryption(False)
+# db.add({'name': 'Mr. Robot', 'start': 2015}, "1")
+# db.set_encryption(True)
+#
+# db.add_many([{'name': 'Shameless', 'start': 2011}, {'name': 'The Boys', 'start': 2019}])
+# db.set_encryption(False)
+# db.add_many([{'name': 'Scrubs', 'start': 2001}, {'name': 'How I Met Your Mother', 'start': 2005}], ("0", "2"))
+# db.set_encryption(True)
+#
+# db.update_by_id("0", {"name": "Truckers"})
+#
+#
+# def func(data):
+#     if data['name'] == 'How I Met Your Mother':
+#         return True
+#
+#
+# db.update_with_function(func, {"name": "Supernatural"})
+
